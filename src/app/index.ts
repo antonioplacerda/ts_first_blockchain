d7b2cc19042795a14ebdb260dea2e1614bdf5747
antonioplacerda <antonioplacerda@gmail.com>
2021-05-19 20:52:33 +0100

    add transactions to API

--- src/app/index.ts
+++ src/app/index.ts
@@ -2,17 +2,23 @@ import express from "express";
 import Blockchain from "../blockchain";
 import bodyParser from "body-parser";
 import P2pServer from "./p2p-server";
+import TransactionPool from "../wallet/transaction-pool";
 
 const HTTP_PORT: number = +(process.env.HTTP_PORT || 3001);
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
 const p2pServer = new P2pServer(blockchain);
 
 app.use(bodyParser.json());
 
 app.get("/blocks", (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.get("/transactions", (req, res) => {
+  res.json(transactionPool.transactions);
 });
 
 app.post("/mine", (req, res) => {
